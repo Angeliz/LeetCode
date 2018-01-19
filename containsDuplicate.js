3b04ea45f8546bd4bf1fa0eaa585531bbb371364
Angeliz <471330418@qq.com>
2018-01-19 17:16:45 +0800

    判断数组中是否有重复项

---
+++ containsDuplicate.js
@@ -0,0 +1,17 @@
+// Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    nums.sort(function(a,b){
+        return a-b;
+    })
+    for(var i=0;i<nums.length-1;i++){
+        if(nums[i]==nums[i+1]){
+            return true;
+        }
+    }
+    return false;
+};
