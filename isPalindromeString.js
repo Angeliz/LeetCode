699ea59c2dde1e7980706588b995191de568a232
Angeliz <471330418@qq.com>
2018-01-29 17:48:01 +0800

    判断一个字符串是否是回文字符串

---
+++ isPalindromeString.js
@@ -0,0 +1,71 @@
+// Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+// For example,
+// "A man, a plan, a canal: Panama" is a palindrome.
+// "race a car" is not a palindrome.
+
+// Note:
+// Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+// For the purpose of this problem, we define empty string as valid palindrome.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s=s.replace(/[^a-zA-Z]/g,"").replace(/[A-Z]/g,function(item){return item.toLowerCase();});
+    var str=s;
+    s=s.split("").reverse().join("");
+    return str==s;
+};
+
+//别人高效的方法
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(input) {
+    var start = 0
+    var end = input.length - 1
+    while (start < end) {
+        var s = input.charCodeAt(start)
+        var e = input.charCodeAt(end)
+    
+        if (!isLetter(s)) {
+            start++
+            continue
+        }
+        if (!isLetter(e)) {
+            end--
+            continue
+        }
+    
+        if (toLowerCase(s) !== toLowerCase(e)) {
+            return false 
+        } 
+        start++
+        end--
+  }
+  return true
+};
+
+var isLetter = function(code) {
+    if (((code >= 48) && (code <= 57))  // numbers
+    || ((code >= 65) && (code <= 90))  // uppercase
+    || ((code >= 97) && (code <= 122))) {  // lowercase
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+var toLowerCase = function(code) {
+    if (code >= 65 && code <= 90) {
+        return code + 32    
+    }
+    else {
+        return code
+    }
+}
