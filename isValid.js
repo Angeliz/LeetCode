ea4f7170580fce98d482f2e52327513eeb075831
Angeliz <471330418@qq.com>
2018-02-04 10:15:03 +0800

    括号适配

--- isValid.js
+++ isValid.js
@@ -6,10 +6,36 @@
  * @param {string} s
  * @return {boolean}
  */
+ //43
 var isValid = function(s) {
     var r=/\{\}|\[\]|\(\)/;
     while(r.test(s)){
         s=s.replace(r,"");
     }
     return !s.length;
+};
+
+//栈 51.8
+var map = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+} 
+
+var isValid = function(s) {
+    var stack = [];
+    
+    for (var i = 0; i < s.length; i++) {
+        var el = s[i];
+        
+        if (map[el]) {
+            stack.push(map[el]);
+        } else {
+            if (el !== stack.pop()) {
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
 };
