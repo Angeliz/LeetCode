0d9fa70044ddb0617b5cf45fbc4b93732bd4bda5
Angeliz <471330418@qq.com>
2018-02-06 12:23:58 +0800

    左上角到右下角，DP

---
+++ uniquePaths.js
@@ -0,0 +1,61 @@
+// A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+// The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+// How many possible unique paths are there?
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+ //73.1
+var uniquePaths = function(m, n) {
+    if(m==1||n==1){
+        return 1;
+    }
+    var result=[];
+    for(let i=0;i<m;i++){
+        result[i]=[];
+    }
+    for(let i=0;i<m;i++){
+        for(let j=0;j<n;j++){
+            if(i==0||j==0){
+              result[i][j] = 1;  
+            } else{
+                 result[i][j]=result[i-1][j]+result[i][j-1]; 
+            }
+           
+        }
+    }
+    return result[m-1][n-1];
+};
+
+//AC算法，java
+// class Solution {
+//     public:
+//         int uniquePaths(int m, int n) {
+//             int N = n + m - 2;// how much steps we need to do
+//             int k = m - 1; // number of steps that need to go down
+//             double res = 1;
+//             // here we calculate the total possible path number 
+//             // Combination(N, k) = n! / (k!(n - k)!)
+//             // reduce the numerator and denominator and get
+//             // C = ( (n - k + 1) * (n - k + 2) * ... * n ) / k!
+//             for (int i = 1; i <= k; i++)
+//                 res = res * (N - k + i) / i;
+//             return (int)res;
+//         }
+//     };
+// First of all you should understand that we need to do n + m - 2 movements : m - 1 down, n - 1 right, because we start from cell (1, 1).
+
+// Secondly, the path it is the sequence of movements( go down / go right),
+// therefore we can say that two paths are different
+// when there is i-th (1 … m + n - 2) movement in path1 differ i-th movement in path2.
+
+// So, how we can build paths.
+// Let’s choose (n - 1) movements(number of steps to the right) from (m + n - 2),
+// and rest (m - 1) is (number of steps down).
+
+// I think now it is obvious that count of different paths are all combinations (n - 1) movements from (m + n-2).
+//  
