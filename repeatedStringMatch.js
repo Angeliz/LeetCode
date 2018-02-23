5cbcff0f1e4a0961ab7524be25742248d81493c1
Angeliz <471330418@qq.com>
2018-02-23 22:35:05 +0800

    字符串重复

---
+++ repeatedStringMatch.js
@@ -0,0 +1,27 @@
+// Given two strings A and B, find the minimum number of times A has to be repeated such that B is a substring of it. If no such solution, return -1.
+
+// For example, with A = "abcd" and B = "cdabcdab".
+
+// Return 3, because by repeating A three times (“abcdabcdabcd”), B is a substring of it; and B is not a substring of A repeated two times ("abcdabcd").
+
+// Note:
+// The length of A and B will be between 1 and 10000.
+
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {number}
+ */
+ // 2.32
+var repeatedStringMatch = function(A, B) {
+    let count=1;
+    let C=A;
+    while(A.length<3*B.length||count<3){
+        if(A.match(B)!=null){
+            return count; 
+        }
+        A+=C;
+        count++;
+    }
+    return -1;
+};
